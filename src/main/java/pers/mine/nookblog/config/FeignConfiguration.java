0659a83efb9a370a9658de83e7331ba8fd52d70e
Mine <yebukong@qq.com>
2019-07-28 22:47:03 +0800

    feign配置

---
+++ src/main/java/pers/mine/nookblog/config/FeignConfiguration.java
@@ -0,0 +1,70 @@
+package pers.mine.nookblog.config;
+
+import feign.Logger;
+import feign.Request;
+import feign.Retryer;
+import feign.codec.Decoder;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.cloud.openfeign.support.SpringDecoder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author yebukong
+ * @description Feign相关配置
+ * @since 2019-07-28 00:18
+ */
+@Configuration
+public class FeignConfiguration {
+    public static int connectTimeOutMillis = 2 * 1000;
+    public static int readTimeOutMillis = 4 * 000;
+
+    @Bean
+    public Request.Options options() {
+        // connectTimeoutMillis=2*1000 链接超时时间
+        // readTimeoutMillis=4*1000 响应超时时间
+        return new Request.Options(connectTimeOutMillis, readTimeOutMillis);
+    }
+
+    /**
+     * NEVER_RETRY 取消重试
+     */
+    @Bean
+    public Retryer feignRetryer() {
+        // period=100 发起当前请求的时间间隔，单位毫秒
+        // maxPeriod=1000 发起当前请求的最大时间间隔，单位毫秒
+        // maxAttempts=5 最多请求次数，包括第一次
+        return new Retryer.Default(100, 1000, 3);
+    }
+
+    @Bean
+    Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
+    }
+
+
+    /**
+     * 支持text/html响应的json
+     */
+    @Bean
+    public Decoder feignDecoder() {
+        ObjectFactory<HttpMessageConverters> objectFactory = () -> new HttpMessageConverters(new MineMessageConverter());
+        return new SpringDecoder(objectFactory);
+    }
+
+}
+
+class MineMessageConverter extends MappingJackson2HttpMessageConverter {
+    public MineMessageConverter() {
+        List<MediaType> mediaTypes = new ArrayList<>();
+        mediaTypes.add(MediaType.TEXT_PLAIN);
+        mediaTypes.add(MediaType.TEXT_HTML);
+        setSupportedMediaTypes(mediaTypes);
+    }
+}
